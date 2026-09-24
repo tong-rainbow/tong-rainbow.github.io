efe13946139701c16bb06fb27575878b50cd0e4d
tong-rainbow <guoweitong20080216@qq.com>
2026-09-24 15:57:43 +0800

    Site updated: 2026-09-24 15:57:41

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1790236170008";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1790236652870";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
